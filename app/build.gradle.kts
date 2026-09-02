f07793fff116e9dbc0c0abf67459c1c6c3e9b854
S I P U N <sipunkumar85@gmail.com>
2026-09-03 00:31:32 +0530

    chore: bump version to 0.5.0
    - Bump version code to 5
    - Set version name to 0.5.0
    
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.darkstar.wallora"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     compileOptions {
